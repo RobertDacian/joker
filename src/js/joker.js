02a2a61fd0e71960650f490977dea3da4a5a5f3e
RobertDacian <tennerdigitalart@gmail.com>
2022-10-26 15:38:35 -0300

    Finished enviromt setup

--- src/js/joker.js
+++ src/js/joker.js
@@ -1,4 +1,3 @@
-import joker from "give-me-a-joke";
-import "robertdacianjoker";
+import robertdacianjoker from "give-me-a-joke";
 
-joker.getRandomDadJoke(console.log);
+robertdacianjoker.getRandomDadJoke(console.log);
